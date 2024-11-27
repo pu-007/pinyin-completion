d04315fe3cc52a75166ee81eb22b980990e14333
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-11-27 14:47:07 +0800

    :recycle: Move _pinyin_comp() to a separate file

--- pinyin-completion.plugin.zsh
+++ pinyin-completion.plugin.zsh
@@ -3,71 +3,12 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-local -A _punctuation_map=(
-	[～]=~
-	[！]=!
-	[＠]=@
-	[＃]='#'
-	[＄]=$
-	[％]=%
-	[＆]='&'
-	[＊]=*
-	[（]='('
-	[）]=')'
-	[＿]=_
-	[－]=-
-	[＋]=+
-	[［]='['
-	[］]=']'
-	[「]='['
-	[」]=']'
-	[【]='['
-	[】]=']'
-	[〔]='['
-	[〕]=']'
-	[＜]='<'
-	[＞]='>'
-	[《]='<'
-	[》]='>'
-	[«]='<'
-	[»]='>'
-	[‹]='<'
-	[›]='>'
-	[？]=?
-	[，]=,
-	[。]=.
-	[／]=/
-	[＼]=\\
-	[、]=\\
-	[…]=...
-)
+PINYIN_REPO_DIR="${0:h}"
+if (( fpath[(I)$PINYIN_REPO_DIR] == 0 )); then
+  fpath+=($PINYIN_REPO_DIR)
+fi
 
-_pinyin_comp()
-{
-  # [:ascii:] need
-  setopt rematchpcre
-
-  # unix file names can contain '\n', so use '\0' to separate them
-  local IFS=$'\0' suffix='' file result k v
-  local -a files
-  if [ "$words[1]" = cd ] ; then
-    suffix=/
-  fi
-  # print -N use '\0' to separate outputs
-  for file in $(print -nN ${1:h}/*"$suffix"); do
-    file="${file#./}"
-    result="$file"
-    if [[ $result =~ [^[:ascii:]] ]]; then
-      for k v in ${(kv)FUZZY} ${(kv)_punctuation_map}; do
-        result="${result//$k/$v}"
-      done
-      result="$(pypinyin -fslug -sz -p= "$result")"
-    fi
-    if [[ $result == $1* ]]; then
-      reply+=(${(q)file})
-    fi
-  done
-}
+autoload -Uz _pinyin_comp
 
 # pinyin-comp is performed as one part of user-expand
 zstyle ':completion:*' user-expand _pinyin_comp
@@ -77,3 +18,5 @@ zstyle ':completion:*:user-expand:*' tag-order expansions
 
 # make use-expand perform as last, when needed
 zstyle ':completion:*' completer _oldlist _expand _complete _match _user_expand
+
+unset PINYIN_REPO_DIR
